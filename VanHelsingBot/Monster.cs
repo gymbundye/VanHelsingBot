443646217a9d7b77dd04a9bf1a43b1db1afc0175
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-10-17 23:16:18 -0400

    added monster class and some logic

---
+++ VanHelsingBot/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanHelsingBot
+{
+    internal class Monster
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Weakness { get; set; }
+        public string? Location { get; set; }
+    }
+}
--- VanHelsingBot/Program.cs
+++ VanHelsingBot/Program.cs
@@ -1,4 +1,38 @@
 ﻿
+using VanHelsingBot;
+
 Console.WriteLine("I am Van Helsing Bot!");
 Console.WriteLine("How may I help you fight the forces of evil?");
-Console.WriteLine("Press 1 for a list of our Monsters.");
+Console.WriteLine("Press 1 to add a Monster to the database.");
+Console.WriteLine("Type exit to end program.");
+
+var userInput = Console.ReadLine();
+
+while (userInput.ToLower() != "exit")
+{
+    if (userInput == "1") 
+    {
+        var monster = new Monster();
+
+        Console.WriteLine("Adding new Monster to the database!");
+
+        Console.WriteLine("What is the name of the fiend?");
+        monster.Name = Console.ReadLine();
+
+        Console.WriteLine("Can you describe the Demon?");
+        monster.Description = Console.ReadLine();
+
+        Console.WriteLine("How can we destroy it?");
+        monster.Weakness= Console.ReadLine();
+
+        Console.WriteLine("Where can we find it?");
+        monster.Location = Console.ReadLine();
+
+
+    }
+    Console.WriteLine("I am Van Helsing Bot!");
+    Console.WriteLine("How may I help you fight the forces of evil?");
+    Console.WriteLine("Press 1 to add a Monster to the database.");
+    Console.WriteLine("Type exit to end program.");
+    userInput = Console.ReadLine();
+}
