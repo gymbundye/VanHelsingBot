10585b3d30a678b622813fed83528156bee8f62d
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-10-18 23:23:17 -0400

    Added Monster Logic to handle adding monsters

---
+++ VanHelsingBot/MonsterLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanHelsingBot
+{
+    internal class MonsterLogic
+    {
+        private List<Monster>? _monsters;
+        private Dictionary<string, Monster>? _monster;
+
+        public MonsterLogic()
+        {
+            _monsters = new List<Monster>();
+            _monster = new Dictionary<string, Monster>();
+        }
+
+        public void AddMonster(Monster monster) 
+        {
+            _monsters?.Add(monster);
+        }
+
+        public List<Monster> GetMonsters() 
+        {
+            return _monsters;
+        }
+
+        public Monster GetMonster(string name) 
+        {
+            return _monster[name];
+        }
+    }
+
+}
--- VanHelsingBot/Program.cs
+++ VanHelsingBot/Program.cs
@@ -1,14 +1,18 @@
 ﻿
+using System.Text.Json;
 using VanHelsingBot;
+
+var monsterLogic = new MonsterLogic();
 
 Console.WriteLine("I am Van Helsing Bot!");
 Console.WriteLine("How may I help you fight the forces of evil?");
 Console.WriteLine("Press 1 to add a Monster to the database.");
+Console.WriteLine("Press 2 to view a Monster");
 Console.WriteLine("Type exit to end program.");
 
 var userInput = Console.ReadLine();
 
-while (userInput.ToLower() != "exit")
+while (userInput?.ToLower() != "exit")
 {
     if (userInput == "1") 
     {
@@ -30,9 +34,18 @@ while (userInput.ToLower() != "exit")
 
 
     }
+    if (userInput == "2") 
+    {
+        Console.WriteLine("Which Monster do we hunt?");
+        var monsterName = Console.ReadLine();
+        var monster = monsterLogic.GetMonster(monsterName);
+        Console.WriteLine(JsonSerializer.Serialize(monster));
+
+    }
     Console.WriteLine("I am Van Helsing Bot!");
     Console.WriteLine("How may I help you fight the forces of evil?");
     Console.WriteLine("Press 1 to add a Monster to the database.");
+    Console.WriteLine("Press 2 to view Monsters.");
     Console.WriteLine("Type exit to end program.");
     userInput = Console.ReadLine();
 }
